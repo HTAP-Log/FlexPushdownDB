7215b313db6dde3e91b3ee74a430e0637eba7802
Yifei Yang <yiy032@eng.ucsd.edu>
2021-02-09 17:19:02 -0800

    Fix build errors for mac

--- normal/cmake/Findbinutils.cmake
+++ normal/cmake/Findbinutils.cmake
@@ -6,9 +6,16 @@ include(FindPackageHandleStandardArgs)
 
 unset(Binutils_INCLUDE_DIR CACHE)
 
-find_path (Binutils_INCLUDE_DIR
-        bfd.h
-        PATHS /usr/include /usr/local/Cellar/binutils/2.35.1/include
-        NO_DEFAULT_PATH)
+if(${APPLE})
+    find_path (Binutils_INCLUDE_DIR
+            bfd.h
+            PATHS /usr/local/opt/binutils/include
+            NO_DEFAULT_PATH)
+else()
+    find_path (Binutils_INCLUDE_DIR
+            bfd.h
+            PATHS /usr/include /usr/local/Cellar/binutils/2.36.1/include
+            NO_DEFAULT_PATH)
+endif()
 
 find_package_handle_standard_args(binutils DEFAULT_MSG Binutils_INCLUDE_DIR)
