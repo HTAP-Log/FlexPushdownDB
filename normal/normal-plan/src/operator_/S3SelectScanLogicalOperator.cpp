5825e69c2c121e1b79b4f75a541c7b6f5549c1b8
r0b3rt24 <hcao29@wisc.edu>
2021-06-01 16:32:55 -0500

    enable update join

--- normal/normal-plan/src/operator_/S3SelectScanLogicalOperator.cpp
+++ normal/normal-plan/src/operator_/S3SelectScanLogicalOperator.cpp
@@ -62,8 +62,8 @@ S3SelectScanLogicalOperator::toOperatorsFullPullup(int numRanges) {
 
     auto operators = std::make_shared<std::vector<std::shared_ptr<normal::core::Operator>>>();
 
-    std::string htapTargetTableName = "lineorder";
-//    std::string htapTargetTableName = "n/a";
+//    std::string htapTargetTableName = "lineorder";
+    std::string htapTargetTableName = "n/a";
 
     /**
      * For each range of each valid partition, create a s3scan (and a filter if needed)
